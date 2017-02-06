6213d140588951c7eb14583a412b4ffb374f608a
1dam.m <1dam.m@PC105.cenecmalaga.net>
2017-02-06 13:13:35 +0100

    Corregido

--- src/Ejercicio33/Ejercicio33.java
+++ src/Ejercicio33/Ejercicio33.java
@@ -1,65 +1,84 @@
 package Ejercicio33;
 
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class Ejercicio33 {
 
-	public static void main(String[] args) {
-		int[] original = {1,2,3,4,5};
-		int[] imprimir = insertarNumero(original);
-		int posicion = 0;
-		
-		for(int insertado : imprimir){
-				System.out.println(insertado);
-		}
-	}
-	public static int validarPosicion(){
-		Scanner sc = new Scanner (System.in);
-		boolean esCorrecto = false;
-		String aux;
-		int posicion = 0;
-		do{
-			System.out.print("Introduce la posición: ");
-			aux = sc.nextLine();
-			try{
-				posicion = Integer.parseInt(aux);
-				esCorrecto = true;
-			}
-			catch(Exception e){
-				System.out.println("No has introducido un número");
-			}
-		}
-		while(!esCorrecto);
-		return posicion;
-	}
-		public static int validarNumero(){
-			Scanner sc = new Scanner (System.in);
-			boolean esCorrecto = false;
-			String aux;
-			int numero = 0;
-			do{
-				System.out.print("Introduce el número: ");
-				aux = sc.nextLine();
-				try{
-					numero = Integer.parseInt(aux);
-					esCorrecto = true;
-				}
-				catch(Exception e){
-					System.out.println("No has introducido un número");
-				}
-			}
-			while(!esCorrecto);
-			return numero;
-		}
-		public static int[] insertarNumero(int[] insertado){
-			int[] array = new int [insertado.length];
-			int posicion = 0, numero=0;
-			
-			for(int i = array.length; i >= posicion; i--) {
-				array[i+1] = array[i];
-			}
-			array[posicion] = numero;
-			return array;
-		}
-	}
 
+		public static void main(String[] args) {
+					int[] array = { 1, 2, 3, 4, 5 };
+					int posicion = validarPosicion();
+					int numero = validarNumero();
+					
+					array = insertarNumero(array, posicion, numero);
+					imprimirArray(array);
+					
+				}
+		
+				public static void imprimirArray(int[] array){
+					for (int resultado : array){
+						System.out.print(resultado+" ");
+					}
+					
+				}
+				
+				public static int validarPosicion() {
+					Scanner sc = new Scanner (System.in);
+					String aux;
+					boolean esNumero = false;
+					int posicion = 0;
+					do {
+						System.out.print("Introduce la posición: ");
+						aux = sc.nextLine();
+						esNumero = validarCifra(aux);
+						if(esNumero) {
+							posicion = Integer.parseInt(aux);
+						}
+					}
+					while(!esNumero);
+					return posicion;
+				}
+				
+				public static int validarNumero() {
+					Scanner sc = new Scanner (System.in);
+					String aux;
+					boolean esNumero = false;
+					int numero = 0;
+					do{
+						System.out.print("Introduce el número que desea insertar: ");
+						aux = sc.nextLine();
+						esNumero = validarCifra(aux);
+						if(esNumero){
+							numero = Integer.parseInt(aux);
+						}
+					}
+					while(!esNumero);
+					return numero;
+				}
+				
+				public static boolean validarCifra(String numero) {
+					boolean esCorrecto = false;
+					try {
+						Integer.parseInt(numero);
+						esCorrecto = true;
+					}
+					catch(Exception e) {
+						System.out.println("No has introducido un número");
+					}
+					return esCorrecto;
+				}
+
+				public static int[] insertarNumero(int[] array, int posicion, int numero) {
+					
+					int[] array_aux = Arrays.copyOf(array, array.length+1);;
+					
+					for(int i = array_aux.length-2; i >= posicion; i--) {
+						array_aux[i+1] = array_aux[i];
+					}
+					array_aux[posicion] = numero;
+					return array_aux;
+				}
+		
+	}
